be9e166b6692f4022f8075b7121fa1ab5b9d4fd5
Mücahit <71331160+mucahitkalabalik@users.noreply.github.com>
2025-01-18 17:36:45 +0300

    Update raceLine.vue

--- components/race/raceLine.vue
+++ components/race/raceLine.vue
@@ -3,11 +3,16 @@
     <h3 class="text-center">Racing Area</h3>
     <div v-if="selectedCars" v-for="(car, index) in selectedCars" :key="index">
       <Driveway
-        v-if="status"
+        v-if="status && currentTour <= 5"
         :car="car"
         :index="index"
         :turLength="tours[currentTour - 1].m"
       />
+    </div>
+    <div v-if="currentTour > 5">
+      <b-card bg-variant="light" class="text-center mt-5">
+        <b-card-text>Race Completed.</b-card-text>
+      </b-card>
     </div>
     <b-row class="mt-3">
       <b-col>
@@ -15,14 +20,18 @@
           class="d-flex aling-center justify-content-center"
           v-if="raceStatus === 'tourfinished'"
         >
-          <b-btn v-if="currentTour < 5" @click="nextTour" variant="danger"
+          <b-btn
+            v-if="currentTour < 5"
+            :disabled="nextButtonStatus"
+            @click="nextTour"
+            variant="danger"
             >Go To Next Round</b-btn
           >
         </div>
       </b-col>
       <b-col v-if="selectedCars">
         <div class="d-flex aling-center justify-content-center">
-          <b>
+          <b v-if="currentTour <= 5">
             {{ tours[currentTour - 1].index }}. Tour -
             {{ tours[currentTour - 1].m }}m</b
           >
@@ -59,10 +68,28 @@ export default {
     raceStatus() {
       return this.$store.getters["race/raceStatus"];
     },
+    nextButtonStatus() {
+      return this.$store.getters["race/getNextButton"];
+    },
   },
   methods: {
+    changenextButton() {
+      const cars = this.$store.state.cars.selectedCars;
+      const slowestCar = cars.reduce((slowest, car) => {
+        return car.speed < slowest.speed ? car : slowest;
+      }, cars[0]);
+
+      const raceDuration =
+        this.tours[this.currentTour - 1].m / slowestCar.speed;
+      setTimeout(() => {
+        this.$store.dispatch("race/changeNextButton", false);
+      }, raceDuration * 1000);
+    },
     nextTour() {
+      this.$store.dispatch("race/changeNextButton", true);
       this.$store.dispatch("race/nextTour");
+      this.$store.dispatch("cars/selectRandomCars");
+      this.$store.dispatch("cars/setProgram");
       this.status = false;
       setTimeout(() => {
         this.status = true;
@@ -70,6 +97,20 @@ export default {
       setTimeout(() => {
         this.$store.dispatch("cars/startRace", true);
       }, 1000);
+      this.changenextButton();
+
+      if (this.currentTour === 5) {
+        const cars = this.$store.state.cars.selectedCars;
+        const slowestCar = cars.reduce((slowest, car) => {
+          return car.speed < slowest.speed ? car : slowest;
+        }, cars[0]);
+
+        const raceDuration =
+          this.tours[this.currentTour - 2].m / slowestCar.speed;
+        setTimeout(() => {
+          this.$store.dispatch("race/nextTour");
+        }, raceDuration * 1300);
+      }
     },
   },
 };
